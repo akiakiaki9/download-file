a3f2ab5dbb9028b085931d2963bf994ab2b6f6c2
Akbar <tillayevakbarshoh@gmail.com>
2026-03-05 12:51:55 +0500

    d

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div>
       <File />
+      <a href='/magnet:?xt=urn:btih:781A39BA6152EEA0A435B8EB0B29974C87323E5E'>
+        <buttom>
+          Скачать
+        </buttom>
+      </a>
     </div>
   );
 }
